98a7d081897d8e5ce3679f7a0634bec952e8c84c
LeNin <lenin.vn.hn@gmail.com>
2024-02-19 15:19:33 +0700

    Passport

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ var usersRouter = require('./routes/users');
 var bodyParser =  require('body-parser');
 var app = express();
 
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
+
 app.use(bodyParser.json());
 
 const url = 'mongodb://localhost:27017/conFusion';
@@ -30,6 +37,132 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+//cookie
+app.use(cookieParser('12345-67890'));
+function auth (req, res, next) {
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');              
+        err.status = 401;
+        next(err);
+        return;
+    }
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+        res.cookie('user','admin',{signed: true});
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');              
+        err.status = 401;
+        next(err);
+    }
+  }
+  else {
+      if (req.signedCookies.user === 'admin') {
+          next();
+      }
+      else {
+          var err = new Error('You are not authenticated!');
+          err.status = 401;
+          next(err);
+      }
+  }
+}
+//session
+app.use(session({
+    name: 'session-id',
+    secret: '12345-67890-09876-54321',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+  }));
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+  function auth (req, res, next) {
+      console.log(req.session);
+  
+      if (!req.session.user) {
+          var authHeader = req.headers.authorization;
+          if (!authHeader) {
+              var err = new Error('You are not authenticated!');
+              res.setHeader('WWW-Authenticate', 'Basic');                        
+              err.status = 401;
+              next(err);
+              return;
+          }
+          var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+          var user = auth[0];
+          var pass = auth[1];
+          if (user == 'admin' && pass == 'password') {
+              req.session.user = 'admin';
+              next(); // authorized
+          } else {
+              var err = new Error('You are not authenticated!');
+              res.setHeader('WWW-Authenticate', 'Basic');
+              err.status = 401;
+              next(err);
+          }
+      }
+      else {
+          if (req.session.user === 'admin') {
+              console.log('req.session: ',req.session);
+              next();
+          }
+          else {
+              var err = new Error('You are not authenticated!');
+              err.status = 401;
+              next(err);
+          }
+      }
+  }
+  
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+function auth (req, res, next) {
+    console.log(req.session);
+
+  if(!req.session.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+  }
+  else {
+    if (req.session.user === 'authenticated') {
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+    }
+  }
+}
+function auth (req, res, next){
+    console.log(req.user)
+    if(!req.user){
+        var err = new Error('You are not authenticated!');
+        err.status = 403
+        next(err);
+
+  }else{
+    next();
+  }
+  }
+  app.use(auth);
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -68,5 +201,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
+
 
 module.exports = app;
